52ca8bf7cf8611e5ff9852ea5e59de0cecf698ee
KrisHD1337 <kriscerni@gmail.com>
2026-03-09 18:36:11 +0100

    Initial commit

---
+++ common/src/main/java/ch/krishd/chunkpermits/Chunkpermits.java
@@ -0,0 +1,9 @@
+package ch.krishd.chunkpermits;
+
+public final class Chunkpermits {
+    public static final String MOD_ID = "chunkpermits";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
---
+++ fabric/src/main/java/ch/krishd/chunkpermits/fabric/client/ChunkpermitsFabricClient.java
@@ -0,0 +1,10 @@
+package ch.krishd.chunkpermits.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class ChunkpermitsFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
+}
